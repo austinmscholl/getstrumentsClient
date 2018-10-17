49921a01200d593503fe004d7f454367555a7bc0
austinmscholl <asprof34@gmail.com>
2018-10-17 15:58:57 -0400

    fixed url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -9,7 +9,7 @@ switch (window.location.hostname) {
     // this is the deployed react application
     case 'getstrumentsclient.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://getstrumentsserver.heroku.app'
+        APIURL = 'https://getstrumentsserver.herokuapp.com'
 }
 
 
